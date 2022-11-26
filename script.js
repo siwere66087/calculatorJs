302f67586907764913b6493d16352abf3666f7c5
siwere66087 <114493677+siwere66087@users.noreply.github.com>
2022-11-26 13:29:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 
 
 
+var form=document.getElementById ("display");
+let btnF = document.querySelectorAll('number');
+let btnS = document.querySelectorAll('operator');
 
-var form=document.getElementById ("for");
-var one = document.getElementById ("one");
-var two =  document.getElementById ("two");
-var three =  document.getElementById ("three");
+
+let one = document.getElementById ("one");
+let two =  document.getElementById ("two");
+let three =  document.getElementById ("three");
 var four =  document.getElementById ("four");
-var five =  document.getElementById ("five");
+const five =  document.getElementById ("five");
 var six =  document.getElementById ("six");
-var seven= document.getElementById ("seven");
+const seven= document.getElementById ("seven");
 var eight =document.getElementById("eight");
-var nine =  document.getElementById ("nine");
+let nine =  document.getElementById ("nine");
 var zero =  document.getElementById ("zero");
 var plus = document.getElementById ("plus");
-var sum = document.getElementById("minus");
-var ap =  document.getElementById ("ap");
+let sum = document.getElementById("minus");
+let ap =  document.getElementById ("ap");
 var doub =  document.getElementById ("double");
-var dd =  document.getElementById ("divide");
-var calc = document.getElementById("got");
+let dd =  document.getElementById ("divide");
+var calc = document.getElementById("sum");
 
 calc.onclick = myFunction;
 
@@ -81,9 +84,145 @@ plus.onclick = ""
 	 ap.onclick = ""
 	 
  }
+ 
+ function backspc() {
+    display.value = display.value.substr(0, display.value.length - 1);
+}
+
+function myFunction1(){
+form.value +="1";
+plus.onclick = myPlus;
+doub.onclick =myDouble;
+
+sum.onclick= myFunction10;
+
+dd.onclick = myFunctiondd;
+
+ap.onclick =myAp;
+
+}
+
+function myFunction2(){
+form.value +="2";
+plus.onclick = myPlus;
+doub.onclick =myDouble;
+
+sum.onclick= myFunction10;
+
+dd.onclick = myFunctiondd;
+
+ap.onclick =myAp;
+}
+
+
+function myFunction3(){
+form.value +="3";
+plus.onclick = myPlus;
+doub.onclick =myDouble;
+
+sum.onclick= myFunction10;
+
+dd.onclick = myFunctiondd;
+
+ap.onclick =myAp;
+}
+function myFunction4(){
+form.value +="4";
+plus.onclick = myPlus;
+doub.onclick =myDouble;
+
+sum.onclick= myFunction10;
+
+dd.onclick = myFunctiondd;
+
+ap.onclick =myAp;
+}
+function myFunction5(){
+form.value +="5";
+
+plus.onclick = myPlus;
+
+doub.onclick =myDouble;
+
+sum.onclick= myFunction10;
+
+dd.onclick = myFunctiondd;
+
+ap.onclick =myAp;
+}
+function myFunction6(){
+form.value +="6";
+plus.onclick = myPlus;
+doub.onclick =myDouble;
+
+sum.onclick= myFunction10;
+
+dd.onclick = myFunctiondd;
+
+ap.onclick =myAp;
+}
+function myFunction7(){
+form.value +="7";
+plus.onclick = myPlus;
+doub.onclick =myDouble;
+
+sum.onclick= myFunction10;
+
+dd.onclick = myFunctiondd;
+
+ap.onclick =myAp;
+}
+
+function myFunction8(){
+form.value +="8";
+plus.onclick = myPlus;
+doub.onclick =myDouble;
+
+sum.onclick= myFunction10;
+
+dd.onclick = myFunctiondd;
+
+ap.onclick =myAp;
+}
+function myFunction9(){
+form.value +="9";
+plus.onclick = myPlus;
+doub.onclick =myDouble;
+
+sum.onclick= myFunction10;
+
+dd.onclick = myFunctiondd;
+
+ap.onclick =myAp;
+}
+
+function myFunction0(){
+form.value +="0";
+plus.onclick = myPlus;
+doub.onclick =myDouble;
+
+sum.onclick= myFunction10;
+
+dd.onclick = myFunctiondd;
+
+ap.onclick =myAp;
+}
 
 
 
+ function myFunction (){
+ var bec = eval(form.value);
+ form.value = bec;
+ plus.onclick = myPlus;
+ doub.onclick =myDouble;
+
+sum.onclick= myFunction10;
+
+dd.onclick = myFunctiondd;
+
+ap.onclick =myAp;
+
+ }
 
 
  
